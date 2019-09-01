1d3e2f7009a6bf66a67d602de8f4df85b38493af
Joel Parker Henderson <joel@joelparkerhenderson.com>
2019-08-31 17:43:15 -0700

    Add places example

---
+++ db/examples/places.sql
@@ -0,0 +1,17 @@
+INSERT INTO places VALUES (
+  CAST('a1b32e52b344acb5187b1749c6697982' as uuid), -- id
+
+  -- Programming-related
+  CAST('7bd380209cd310d3ad4e7f980298cbea' as uuid), -- tenant_id
+  '', -- type
+  '', -- state
+
+  -- Update-related
+  TO_TIMESTAMP('2020-01-01 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), -- updated_at_timestamp_utc
+  0, -- updated_at_clock_counter
+  'updated by example', -- updated_by_text
+
+  -- Name-related
+  'Grand Central Station in New York City', -- name
+
+);
