14e7d0a16d85928179a575434fa41902d2414227
Joel Parker Henderson <joel@joelparkerhenderson.com>
2019-08-31 17:43:14 -0700

    Add tags migration

---
+++ db/migrations/10000000000002_create_tags.sql
@@ -0,0 +1,38 @@
+--liquibase formatted sql
+-- see: http://www.liquibase.org/
+
+--changeset contact@contactopensource.com:3
+--preconditions onFail:HALT onError:HALT
+--precondition-sql-check expectedResult:0 SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'tags'
+
+CREATE TABLE tags (
+  id uuid not null primary key,
+
+  -- Programming-related
+  tenant_id uuid, -- for optional multi-tenant installation
+  typecast text, -- for optional single table inheritance
+  state text, -- for optional state machine transition
+
+  -- Update-related
+  updated_at_timestamp_utc timestamp, -- example: 2020-01-01T00:00:00 always UTC
+  updated_at_clock_counter bigint, -- example: 123456789 as suitable for a vector clock
+  updated_by_text text, -- example: explanation of who updated the contact, why, how, etc.
+
+  -- Tag-related
+  text text -- example: "trending"
+}
+
+-- Programming-related
+CREATE INDEX ix_tags_tenant_id on tags(tenant_id);;
+CREATE INDEX ix_tags_typecast on tags(typecast);
+CREATE INDEX ix_tags_state on tags(state);
+
+-- Update-related
+CREATE INDEX ix_tags_updated_at_timestamp_utc on tags(updated_at_timestamp_utc);
+CREATE INDEX ix_tags_updated_at_clock_counter on tags(updated_at_clock_counter);
+CREATE INDEX ix_tags_updated_by_text on tags(updated_by_text);
+
+-- General-related
+CREATE INDEX ix_tags_text on tags(text);
+
+--rollback drop table tags;
